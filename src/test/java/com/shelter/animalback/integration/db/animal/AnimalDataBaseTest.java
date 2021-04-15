2b02f4cb9274e83eae34fda029a19bd10dbe8279
Mayumi Tamura <maythermyself@gmail.com>
2021-04-14 20:46:36 -0500

    AnimalDataBaseTest done and working

--- src/test/java/com/shelter/animalback/integration/db/animal/AnimalDataBaseTest.java
+++ src/test/java/com/shelter/animalback/integration/db/animal/AnimalDataBaseTest.java
@@ -5,14 +5,24 @@ import com.shelter.animalback.repository.AnimalRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = { "spring.config.additional-location=classpath:integration-test.yml"})
+@Testcontainers
 public class AnimalDataBaseTest {
+
+    @Container
+    private static PostgreSQLContainer database = new PostgreSQLContainer("postgres:13.2");
 
     @Autowired
     private AnimalRepository animalRepository;
@@ -23,5 +33,16 @@ public class AnimalDataBaseTest {
         var animalDb = animalRepository.save(animal);
 
         assertThat(animalDb, notNullValue());
+        assertThat(animalDb.getName(), equalTo("Hela"));
+        assertThat(animalDb.getBreed(), equalTo("Mestizo"));
+        assertThat(animalDb.getGender(), equalTo("Female"));
+        assertThat(animalDb.isVaccinated(), equalTo(true));
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
     }
 }
